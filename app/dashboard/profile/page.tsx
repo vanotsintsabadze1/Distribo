03f4a9e035b04ee8d28aff0e9b229d30d6291ac9
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-09-27 16:25:29 +0400

    Move getCompany method to a separate file

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -3,42 +3,19 @@ import Profile from "@/components/profile/Profile";
 import PageLayoutComp from "@/components/ui/PageLayoutComp";
 import { getUserAuthStatus } from "@/lib/actions/auth/auth";
 import { getUserRole } from "@/lib/actions/helpers/encodeUserCredentials";
-import { getUserToken } from "@/lib/actions/helpers/getUserToken";
-import { API_URL } from "@/lib/constants/constants";
-
-async function getCompany() {
-  const token = await getUserToken();
-
-  try {
-    const res = await fetch(`${API_URL}/v1/Company`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      cache: "no-cache",
-    });
-
-    const data = await res.json();
-
-    // prettier-ignore
-    return res.ok ? { status: 200, message: "Successfully fetched the companies", data } : { status: res.status, message: res.statusText, data: null };
-  } catch (error) {
-    console.error(error);
-    return { status: 500, message: "Internal Server Error", data: null };
-  }
-}
+import { getCompanyOrders } from "@/lib/actions/orders/getCompanyOrders";
 
 export default async function ProfilePage() {
   const userData = await getUserAuthStatus();
   const userEmail = userData.data?.email;
-  const userRole = await getUserRole()
-  const companiesData = await getCompany();
+  const userRole = await getUserRole();
+  const companiesData = await getCompanyOrders(0, 1);
   const company = companiesData ? companiesData.data : null;
 
   return (
     <PageLayoutComp title="Profile" description="Update your profile information.">
       <CompanyCreationNavigatorButton />
-      {userRole && userEmail !== undefined && <Profile userEmail={userEmail} userRole={userRole} company={company}/>}
+      {userRole && userEmail !== undefined && <Profile userEmail={userEmail} userRole={userRole} company={company} />}
     </PageLayoutComp>
   );
 }
