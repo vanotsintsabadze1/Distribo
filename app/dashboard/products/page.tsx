6bbfef7a7a62a0c8fb5352968269863015f8d79a
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-10-04 17:27:30 +0400

    Remove the button visibility for unauthorized users

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -2,14 +2,17 @@ import PageLayoutComp from "@/components/ui/PageLayoutComp";
 import ProductCreationNavigatorButton from "@/components/products/ProductCreationNavigatorButton";
 import ProductsWrapper from "@/components/products/ProductsWrapper";
 import { getAllProducts } from "@/lib/actions/products/getProducts";
+import { getUserRole } from "@/lib/actions/helpers/encodeUserCredentials";
 
 export default async function ProductsPage() {
   const data = await getAllProducts();
   const products = data ? data.data : null;
+  const role = await getUserRole();
+  const isAdmin = role === "Admin" || role === "Employee";
 
   return (
     <PageLayoutComp title="Products" description="All the products are listed below.">
-      <ProductCreationNavigatorButton />
+      {isAdmin && <ProductCreationNavigatorButton />}
       <ProductsWrapper products={products} />
     </PageLayoutComp>
   );
