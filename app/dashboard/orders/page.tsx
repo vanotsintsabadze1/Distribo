155c8d5142a7a71b232c921a62a5b187f77b07c3
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-10-05 16:50:29 +0400

    Fix users being able to see admin orders panel

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,3 +1,4 @@
+import PageAuthenticator from "@/components/auth/PageAuthenticator";
 import SingleCompanyOrdersWrapper from "@/components/orders/SingleCompanyOrdersWrapper";
 import PageLayoutComp from "@/components/ui/PageLayoutComp";
 import { getUserRole } from "@/lib/actions/helpers/encodeUserCredentials";
@@ -5,11 +6,13 @@ import { getAllCompaniesOrders } from "@/lib/actions/orders/getAllCompaniesOrder
 
 export default async function OrdersPage() {
   const orders = await getAllCompaniesOrders(0, 1, 10);
-  const role = await getUserRole()
+  const role = await getUserRole();
 
   return (
-    <PageLayoutComp title="Orders" description="All the orders are listed below.">
-      <SingleCompanyOrdersWrapper orders={orders.data} role={role}/>
-    </PageLayoutComp>
+    <PageAuthenticator shouldAllow="admin" redirectTo="/dashboard/profile/my-company?orders=true">
+      <PageLayoutComp title="Orders" description="All the orders are listed below.">
+        <SingleCompanyOrdersWrapper orders={orders.data} role={role} />
+      </PageLayoutComp>
+    </PageAuthenticator>
   );
 }
