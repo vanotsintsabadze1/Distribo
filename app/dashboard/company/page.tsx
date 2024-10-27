4e64b6c68a4f3b64a52e92d68b92b7914d181558
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-10-27 17:30:59 +0400

    Remove companies button from the navigation bar for non-staff users

--- app/dashboard/company/page.tsx
+++ app/dashboard/company/page.tsx
@@ -1,11 +1,10 @@
+import PageAuthenticator from "@/components/auth/PageAuthenticator";
 import PageLayoutComp from "@/components/ui/PageLayoutComp";
 
-
-
 export default async function CompanyPage() {
-  
   return (
-    <PageLayoutComp title="Companies" description="All the created companies are listed below.">
-    </PageLayoutComp>
+    <PageAuthenticator shouldAllow="admin" redirectTo="/dashboard">
+      <PageLayoutComp title="Companies" description="All the created companies are listed below."></PageLayoutComp>
+    </PageAuthenticator>
   );
 }
