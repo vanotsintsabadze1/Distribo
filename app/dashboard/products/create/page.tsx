284f4c1cadd87e679cbb9d33bd6d0d739b0f1986
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-10-27 17:46:57 +0400

    Prevent employees from creating products

--- app/dashboard/products/create/page.tsx
+++ app/dashboard/products/create/page.tsx
@@ -3,7 +3,7 @@ import ProductCreationForm from "@/components/products/ProductCreationForm";
 
 export default function ProductCreationPage() {
   return (
-    <PageAuthenticator shouldAllow="admin" redirectTo="/dashboard/products">
+    <PageAuthenticator shouldAllow="admin" shouldNotAllowEmployee redirectTo="/dashboard/products">
       <div className="flex w-full flex-col items-center justify-center gap-14 px-4 py-5">
         <div className="flex w-full flex-col gap-1.5">
           <h1 className="text-2xl font-semibold">Create a new product</h1>
